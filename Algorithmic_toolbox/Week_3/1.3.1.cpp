cee3ebacf0713fe508cd3735e8d340dd63178ce9
ivk22 <43445072+ivk22@users.noreply.github.com>
2019-07-22 08:54:23 +0300

    Add files via upload

---
+++ Algorithmic_toolbox/Week_3/1.3.1.cpp
@@ -0,0 +1,40 @@
+// 1.3.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	size_t n = 0;
+	cin >> n;
+	size_t res = 0;
+	while (n) {
+		if (n >= 10) {
+			n -=10;
+
+		}
+		else if (n >= 5) {
+			n -= 5;
+		}
+		else if (n >= 1) {
+			--n;
+		}
+		else {
+			break;
+		}
+		++res;
+	}
+	cout << res;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
